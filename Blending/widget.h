d564aa70e27ef3fc730f9b4f02e1914d0730dcc8
Seungchul Yu <32051415+SeungchulDave@users.noreply.github.com>
2022-12-21 13:56:44 +0900

    Add files via upload

---
+++ Blending/widget.h
@@ -0,0 +1,31 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QtOpenGL>
+#include <QtOpenGLWidgets>
+
+class Widget : public QOpenGLWidget, protected  QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+Widget(QWidget *parent = 0);
+~Widget();
+
+GLfloat alpha;
+GLenum src;
+GLenum dest;
+
+protected:
+    void keyPressEvent(QKeyEvent* event);
+
+
+private:
+    void initializeGL();
+    void paintGL();
+    void resizeGL(int w, int h);
+public slots:
+
+};
+#endif // WIDGET_H
