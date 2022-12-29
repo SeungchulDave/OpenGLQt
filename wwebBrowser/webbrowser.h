1f320b01f90f48dd0c625003762456dc889923da
Seungchul Yu <32051415+SeungchulDave@users.noreply.github.com>
2022-12-29 14:45:20 +0900

    Add files via upload

---
+++ wwebBrowser/webbrowser.h
@@ -0,0 +1,25 @@
+#ifndef WEBBROWSER_H
+#define WEBBROWSER_H
+
+#include <QMainWindow>
+#include <QWebEngineView>
+
+class QLineEdit;
+
+class WebBrowser : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    WebBrowser(QWidget *parent = nullptr);
+    ~WebBrowser();
+private:
+    QLineEdit *lineEditURL;
+    QWebEngineView *webEngineView;
+public slots:
+    void goSlot();
+    void goBackwardSlot();
+    void goForwardSlot();
+    void goHomeSlot();
+};
+#endif // WEBBROWSER_H
